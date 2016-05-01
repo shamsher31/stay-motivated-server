79715f2a9c7003338cc29ca8dfc541cf7ddf083e
shamsher31 <ansarisam47@gmail.com>
2016-05-01 23:55:16 +0530

    Create routes for qoutes

--- server.go
+++ server.go
@@ -2,23 +2,87 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/fasthttp"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Quote struct {
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Title     string        `json:"title" bson:"title"`
+	Author    string        `json:"author" bson:"author"`
+	Timestamp time.Time     `json:"timestamp" bson:"timestamp"`
+}
+
+func dbConnection() session {
+  session, err := mgo.Dial("mongodb://127.0.0.1")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+  
+  return session
+}
+
+func generateId() id {
+  id := bson.NewObjectId()
+  return id
 }
 
 func main() {
 
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		qoute := Quote{"Education is the key to success", "Sam"}
+  
+  session := dbConnection()
+  
+  e.POST("/qoutes", createQoute)
+  e.GET("/qoutes/", getAllQoutes)
+  e.GET("/qoutes/:id", getQoute)
+  e.PUT("/qoutes/:id", updateQoute)
+  e.DELETE("/qoutes/:id", deleteQoute)
 
-		return c.JSON(http.StatusOK, qoute)
-	})
-	e.Run(fasthttp.New(":4000"))
+  e.Run(fasthttp.New(":4000"))
+  
+  func createQoute(c echo.Context) error {
+      
+      title := c.FormValue("title")
+      author := c.FormValue("author")
+      
+      c := session.DB("motivational").C("qoutes")
+      id := generateId()
+      err = c.Insert(&Quote{id, "Education is the key to success", "Sam", time.Now()})
+      if err != nil {
+        log.Fatal(err)
+      }
+      
+      return c.JSON(http.StatusOK)
+  }
+  
+  func getAllQoutes(c echo.Context) {
+    var results []Quote
+	  err = c.Find(bson.M{"name": "Sam"}).Sort("-timestamp").All(&results)
+
+	 if err != nil {
+		 panic(err)
+	 }
+   
+   return c.JSON(http.StatusOK, results)
+  }
+  
+  func getQoute(c echo.Context)  {
+    result := Quote{}
+	  err = c.Find(bson.M{"name": "Sam"}).One(&result)
+	  if err != nil {
+		  panic(err)
+	  }
+    
+    return c.JSON(http.StatusOK, result)
+  }
 }
