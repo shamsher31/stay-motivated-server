2a23cc4381926b8fe07959ddac5a537c7c586aea
shamsher31 <ansarisam47@gmail.com>
2016-10-05 12:55:07 +0530

    Use middleware and update dependency

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
-	//"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/middleware"
 )
 
 func init() {
@@ -22,8 +22,8 @@ func main() {
 	e := echo.New()
 
 	// Middleware
-	// e.Use(middleware.Logger())
-	// e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.POST("/qoutes/", CreateQoute)
 	e.GET("/qoutes/", GetAllQoutes)
